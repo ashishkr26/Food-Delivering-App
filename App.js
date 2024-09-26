7a09381b5ac7017ea9d6a3ede8df91cd6abfe381
ashishkr26 <ashish.dypcoe@gmail.com>
2024-09-26 13:50:14 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = document.createElement("div");
+heading.innerHTML = "Hello World From Javascript";
+const root = document.getElementById("root");
+root.appendChild(heading);
+
+const parent = React.createElement(
+  "div",
+  {id:'parent'},
+ [React.createElement("h1", {id:'child'}, "I am react Component"),
+    React.createElement("h2", {}, "I am h2 Tag ")
+ ]
+);
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(parent);
